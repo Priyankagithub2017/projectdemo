e3cd67af54fa9d1973e89caab9c9314abc054e47
priyanka <priyankagulbhile@gmail.com>
2023-01-18 18:45:20 +0530

    Successful

---
+++ src/project2/PrintDemo.java
@@ -0,0 +1,14 @@
+package project2;
+
+import java.util.ArrayList;
+
+public class PrintDemo {
+
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("java");
+		al.add("String");
+		System.out.println(al);
+	}
+
+}
